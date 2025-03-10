15ef59295a95c078104a4bb26c3d2167ab0f27d6
margit56 <73655705+margit56@users.noreply.github.com>
2025-03-10 21:08:42 +0100

    Add files via upload

---
+++ TSSEurekaServerProfile/src/test/java/com/tss/TSSEurekaServer/TssEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.TSSEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TssEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
